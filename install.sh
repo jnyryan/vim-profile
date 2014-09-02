ce47b83f2b9c9a35ea2cdba5ff5a08887e7e9ad8
John Ryan <johnryan_irl@yahoo.com>
2014-09-02 14:22:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,3 +25,5 @@ git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 #################################
 echo installing Numbers
 git clone https://github.com/myusuf3/numbers.vim.git ~/.vim/bundle/numbers
+
+wget https://raw.githubusercontent.com/jnyryan/vim-profile/master/.vimrc ~/.vimrc
