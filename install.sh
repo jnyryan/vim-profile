5b733cc41e572f934809604a6b08344269d9b829
John Ryan <johnryan_irl@yahoo.com>
2014-09-02 11:04:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #################################
 echo installing NERDtree
 mkdir -p ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
